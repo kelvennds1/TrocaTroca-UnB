1c784114d6ea030b8e5966b1bfbfa485e9e7ba28
kelvennds1 <kelvencrstn@gmail.com>
2023-07-06 13:23:48 -0300

    Inserir

--- routes/inserir_route.py
+++ routes/inserir_route.py
@@ -28,21 +28,18 @@ def inserir():
                 brand_species = request.form['race']
             condition = request.form['condition']
             year = request.form['year']
-    
-            
-            new_item = Item(person_idperson = user, name = title, brand_species = brand_species, year_acquired = year, desc = description, condition = condition, category_idcategory = category)
+            file = request.files['photos']
+            image_blob = file.read()
+            new_item = Item(person_idperson = user, name = title, brand_species = brand_species, year_acquired = year, desc = description, condition = condition, category_idcategory = category, image_blob=image_blob)
     
             #if 'photos' in request.files:
-            file = request.files['photos']
+            
 
             # Acessar os atributos do arquivo
-            filename = file.filename
-            file_type = file.content_type
-            file_data = file.read()
+           
 
             # Salvar o conteúdo binário da imagem no atributo image_blob do objeto Item
-            new_item.image_blob = file_data
-
+    
             session_bd.add(new_item)
             session_bd.commit()
         return render_template('inserir_anuncio.html', anos=anos, categorias = categorias)
