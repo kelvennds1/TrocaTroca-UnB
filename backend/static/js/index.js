6f19036e1c5a36422452ae33b27fcf19fb054d41
Kelven Silva <kelvencrstn@gmail.com>
2024-09-28 01:05:40 -0300

    update: excluindo arquivos do antigo backend

--- backend/static/js/index.js
+++
@@ -1,6 +0,0 @@
-// Aguarde 3 segundos (3000 milissegundos)
-setTimeout(function() {
-    // Redirecione para a página de login
-    window.location.href = "login";
-  }, 1000);
-  
--- backend/static/js/sidebar.js
+++
@@ -1,6 +0,0 @@
-const hamburgerIcon = document.getElementById("hamburgerIcon");
-const sidebar = document.querySelector(".sidebar");
-
-hamburgerIcon.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
