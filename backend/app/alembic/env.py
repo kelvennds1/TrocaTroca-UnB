bae5fb5be2de36579c7e80b8f45ff01b01e84c06
Kelven Silva <kelvencrstn@gmail.com>
2024-09-28 01:52:41 -0300

    fix: Caminho da main

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from backend.main import DATABASE_URL  # importe a URL do banco de dados
+from backend.app.main import DATABASE_URL  # importe a URL do banco de dados
 from models import Base
 
 
